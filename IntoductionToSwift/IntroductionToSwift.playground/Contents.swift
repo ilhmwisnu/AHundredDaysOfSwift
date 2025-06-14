36300c0e4714e26855007d86cbac2d4cccfebb2e
Ilham Wisnu <ilhamwisnu42@gmail.com>
2025-06-14 10:16:13 +0700

    day 1-2

---
+++ IntoductionToSwift/IntroductionToSwift.playground/Contents.swift
@@ -0,0 +1,85 @@
+var greeting = "Hello, playground"
+
+greeting = "Hello, World!"
+
+let age: Int = 23
+
+var name: String = "Wisnu"
+var fileName: String = "hello.swift"
+
+print(name.count)
+
+print(name.uppercased())
+
+print(fileName.hasSuffix(".swift"))
+
+var score = 10
+
+var reallyBigNumber = 1_000_000_000
+
+var lowerScore = score - 1
+var higherScore = score + 1
+var doubledScore = score * 2
+var squaredScore = score * score
+var halfScore = score / 2
+print(score)
+
+var counter = 1
+
+counter = counter + 1
+
+counter += 1
+
+print(counter)
+
+counter *= 2
+
+counter /= 2
+
+print(counter)
+
+print(counter.isMultiple(of: 3))
+
+var a = 1
+var b = 2.0
+
+var c = a + Int(b)
+var d = Double(a) + b
+
+var rating = 4.2
+
+rating *= 2
+
+var isAuthenticated: Bool = true
+
+isAuthenticated = !isAuthenticated
+
+isAuthenticated.toggle()
+
+var firstName = "Ilham"
+var lastName = "Wisnu"
+
+var fullName = firstName + " " + lastName
+
+print(fullName)
+
+name = "Taylor"
+let agee = 26
+let message = "Hello, my name is \(name) and I'm \(agee) years old."
+
+print(message)
+
+print("5 x 5 is \(5 * 5)")
+
+var celciusDegree: Double = 25.0
+
+var fahrenheitDegree: Double = (celciusDegree * 9 / 5) + 32
+
+print("\(celciusDegree)° Celcius is equal to \(fahrenheitDegree)° Fahrenheit.")
+
+
+
+
+
+
+
