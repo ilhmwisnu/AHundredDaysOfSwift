a7f7f52f83d5a6217539f1c5e2b7b3f708b891e8
Ilham Wisnu <ilhamwisnu42@gmail.com>
2025-07-01 21:42:29 +0700

    day 6

--- IntoductionToSwift/IntroductionToSwift.playground/Pages/CheckpointThree.xcplaygroundpage/Contents.swift
+++ IntoductionToSwift/IntroductionToSwift.playground/Pages/CheckpointThree.xcplaygroundpage/Contents.swift
@@ -174,3 +174,100 @@ let canVote = age >= 18 ? "Yes" : "No"
 
 let hour = 23
 print(hour < 12 ? "It's before noon" : "It's after noon")
+
+
+let platforms = ["macOS", "iOS", "tvOS", "watchOS"]
+
+for platform in platforms {
+    print(platform)
+}
+
+for i in 1...12 {
+    print("The \(i) times table:")
+
+    for j in 1...12 {
+        print("  \(j) x \(i) is \(j * i)")
+    }
+
+    print()
+}
+
+for i in 1...5 {
+    print("Counting from 1 through 5: \(i)")
+}
+
+print()
+
+for i in 1..<5 {
+    print("Counting 1 up to 5: \(i)")
+}
+
+
+var lyric = "Haters gonna"
+
+for _ in 1...5 {
+    lyric += " hate"
+}
+
+print(lyric)
+
+var countdown = 10
+
+while countdown > 0 {
+    print("\(countdown)…")
+    countdown -= 1
+}
+
+print("Blast off!")
+
+// create an integer to store our roll
+var roll = 0
+
+// carry on looping until we reach 20
+while roll != 20 {
+    // roll a new dice and print what it was
+    roll = Int.random(in: 1...20)
+    print("I rolled a \(roll)")
+}
+
+// if we're here it means the loop ended – we got a 20!
+print("Critical hit!")
+
+let filenames = ["me.jpg", "work.txt", "sophie.jpg", "logo.psd"]
+
+for filename in filenames {
+    if filename.hasSuffix(".jpg") == false {
+        continue
+    }
+
+    print("Found picture: \(filename)")
+}
+
+let number1 = 4
+let number2 = 14
+var multiples = [Int]()
+
+for i in 1...100_000 {
+    if i.isMultiple(of: number1) && i.isMultiple(of: number2) {
+        multiples.append(i)
+
+        if multiples.count == 10 {
+            break
+        }
+    }
+}
+
+print(multiples)
+
+
+for i in 1...100 {
+    if i.isMultiple(of: 3) && i.isMultiple(of: 5)  {
+        print("FizzBuzz")
+    } else if i.isMultiple(of: 3)  {
+        print("Fizz")
+    } else if i.isMultiple(of: 5) {
+        print("Buzz")
+    } else {
+        print(i)
+    }
+}
