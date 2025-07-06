172f4b06e67ad98c288ddb2eb4ea2a603b957d77
Ilham Wisnu <ilhamwisnu42@gmail.com>
2025-07-06 20:14:37 +0700

    day 8

--- IntoductionToSwift/IntroductionToSwift.playground/Pages/CheckpointFour.xcplaygroundpage/Contents.swift
+++ IntoductionToSwift/IntroductionToSwift.playground/Pages/CheckpointFour.xcplaygroundpage/Contents.swift
@@ -78,3 +78,75 @@ print("Name: \(firstName) \(lastName)")
 
 let (firstName2, _) = getUser2()
 print("Name: \(firstName)")
+
+func isUppercase(_ string: String) -> Bool {
+    string == string.uppercased()
+}
+
+let string = "HELLO, WORLD"
+let isUppercaseLet = isUppercase(string)
+
+func printTimesTables(for number: Int, end: Int = 12) {
+    for i in 1...end {
+        print("\(i) x \(number) is \(i * number)")
+    }
+}
+
+printTimesTables(for: 5, end: 20)
+printTimesTables(for: 8)
+
+enum PasswordError: Error {
+    case short, obvious
+}
+
+func checkPassword(_ password: String) throws -> String {
+    if password.count < 5 {
+        throw PasswordError.short
+    }
+
+    if password == "12345" {
+        throw PasswordError.obvious
+    }
+
+    if password.count < 8 {
+        return "OK"
+    } else if password.count < 10 {
+        return "Good"
+    } else {
+        return "Excellent"
+    }
+}
+
+let pass = "12345"
+
+do {
+    let result = try checkPassword(pass)
+    print("Password rating: \(result)")
+} catch PasswordError.short {
+    print("Please use a longer password.")
+} catch PasswordError.obvious {
+    print("I have the same combination on my luggage!")
+} catch {
+    print("There was an error.")
+}
+
+enum CustomError : Error {
+    case outOfBounds
+    case noRoot
+}
+
+func squareRoot(_ number: Int) throws -> Int {
+    if number < 1 || number > 10_000 {
+        throw CustomError.outOfBounds
+    }
+    
+    for i in 1...100 {
+        if (i * i) == number {
+            return i
+        }
+    }
+    
+    throw CustomError.noRoot
+}
+
+try print(squareRoot(3))
